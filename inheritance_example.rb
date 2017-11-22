a13c252391bd7b27f0fe1d205263a82fa05c04b5
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-11-21 21:18:59 -0600

    Create method for bike and car

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,51 @@
-class Car
+# class Car
+#   # def initialize
+#   #   @speed = 0
+#   #   @direction = 'north'
+#   # end
+
+#   # def brake
+#   #   @speed = 0
+#   # end
+
+#   # def accelerate
+#   #   @speed += 10
+#   # end
+
+#   # def turn(new_direction)
+#   #   @direction = new_direction
+#   # end
+
+#   def honk_horn
+#     puts "Beeeeeeep!"
+#   end
+# end
+
+# class Bike
+  # def initialize
+  #   @speed = 0
+  #   @direction = 'north'
+  # end
+
+  # def brake
+  #   @speed = 0
+  # end
+
+  # def accelerate
+  #   @speed += 10
+  # end
+
+  # def turn(new_direction)
+  #   @direction = new_direction
+  # end
+
+#   def ring_bell
+#     puts "Ring ring!"
+#   end
+# end
+
+class Transportation
+
   def initialize
     @speed = 0
     @direction = 'north'
@@ -15,31 +62,26 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
-  def honk_horn
+class Car < Transportation
+    def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Transportation
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+car1 = Car.new
+
+car1.honk_horn
+car1.accelerate
+
+bike1 = Bike.new
+
+bike1.ring_bell
+bike1.accelerate
