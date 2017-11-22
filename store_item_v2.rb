97756a7cdedfec5775c6ccc7a7acf735ed157624
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-11-21 20:27:14 -0600

    Change code in file

---
+++ store_item_v2.rb
@@ -0,0 +1,34 @@
+class Shopping_List
+  def initialize (input_item_name, input_item_color, input_item_origin, input_item_price)
+
+    @item_name = input_item_name
+    @item_color = input_item_color
+    @item_origin = input_item_origin
+    @item_price = input_item_price
+  end
+
+  def item_name
+    @item_name
+  end
+
+  def item_color
+    @item_color
+  end
+
+  def item_origin
+    @item_origin
+  end
+
+  def item_price
+    @item_price
+  end
+
+  def print_info
+    "The #{item_name} is #{item_color}. It is from #{item_origin} and costs $#{item_price} per pound."
+  end
+end
+
+  item1 = Shopping_List.new("orange", "orange", "florida", "0.50")
+
+  p item1.print_info
+
