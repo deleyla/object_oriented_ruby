b562d11ad23ad05a1a8e0724d8566b1fdcec89d4
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-11-26 13:40:09 -0600

    Add comment retrieval code

--- reddit_app.rb
+++ reddit_app.rb
@@ -1,16 +1,3 @@
-# require 'unirest'
-# p "Here are the posts from the AdOps SubReddit:"
-# while comment = gets.chomp
-#   case comment
-#   when "q"
-#     p "Thank you for using the Reddit Comment App"
-#     break
-#   else
-#     response = Unirest.get("https://www.reddit.com/r/adops/.json")
-#     reddit_comment = response.body
-#     comment = reddit_comment[]
-#     p "What comment would you like to view? Enter a number."
-#     comment_number = gets.chomp
 
 #Get subreddit page
 require 'unirest'
@@ -32,9 +19,27 @@ p "#{article_number}. #{article_title}"
   i += 1
 end
 
+# alternative way to loop
 # posts_to_select_from.each do |post|
 #   p post["data"]["selftext"]
 # end
 
 #Get comments from selected post
+#open url
 
+# response = Unirest.get("https://www.reddit.com/r/adops/comments/.json")
+# post_list = response.body
+
+# #List post names
+
+# posts_to_select_from = post_list["data"]["children"]
+
+# number_of_loops = posts_to_select_from.length
+
+# i = 1
+# number_of_loops.times do
+# article_number = i.to_s
+# article_title = posts_to_select_from[i]["data"]["link_title"]
+# p "#{article_number}. #{article_title}"
+#   i += 1
+# end
