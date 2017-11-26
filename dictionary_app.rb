f50c5a6952d06696647a147abdb71ddde7b771b0
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-11-26 11:55:03 -0600

    Change response names to differentiate between them

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -9,21 +9,21 @@ while word = gets.chomp
     p "Thank you for using the app."
     break
   else
-    response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    definition_response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
     dictionary_definition = response.body
     definition = dictionary_definition[0]["text"]
 
     p "The definition of the word you requested is: " + definition
 
-    response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    examples_response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
     dictionary_examples = response.body
     top_example = dictionary_examples["examples"][0]["text"]
 
     p "Here is an example of how you can utilize the word you requested: " + top_example
 
-    response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    pronunciation_response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
     dictionary_pronunciation = response.body
     pronunciation = dictionary_pronunciation[0]["raw"]
