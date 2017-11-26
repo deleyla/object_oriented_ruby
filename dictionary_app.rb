5b68652b4fe4c18c95d94492dfe70ed0111a9702
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-11-26 11:49:56 -0600

    Add while loop and break

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,25 +1,38 @@
 require 'unirest'
 
 p "What word would you like information on?"
-word = gets.chomp
 
-response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+while word = gets.chomp
+  case word
 
-dictionary_definition = response.body
-definition = dictionary_definition[0]["text"]
+  when "q"
+    p "Thank you for using the app."
+    break
+  else
+    response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-p "The definition of the word you requested is: " + definition
+    dictionary_definition = response.body
+    definition = dictionary_definition[0]["text"]
 
-response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    p "The definition of the word you requested is: " + definition
 
-dictionary_examples = response.body
-top_example = dictionary_examples["examples"][0]["text"]
+    response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-p "Here is an example of how you can utilize the word you requested: " + top_example
+    dictionary_examples = response.body
+    top_example = dictionary_examples["examples"][0]["text"]
 
-response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+    p "Here is an example of how you can utilize the word you requested: " + top_example
 
-dictionary_pronunciation = response.body
-pronunciation = dictionary_pronunciation[0]["raw"]
+    response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-p "Here is how you pronounce the word you requested: " + pronunciation
+    dictionary_pronunciation = response.body
+    pronunciation = dictionary_pronunciation[0]["raw"]
+
+    p "Here is how you pronounce the word you requested: " + pronunciation
+  end
+p "Please enter another word or enter q to quit"
+end
+
+
+
+
