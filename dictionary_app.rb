32505ea2ea226396f3c1d3d67a2643f89452f632
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2017-11-26 12:05:06 -0600

    Alter response.body to differentiate between different response types

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -11,21 +11,21 @@ while word = gets.chomp
   else
     definition_response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-    dictionary_definition = response.body
+    dictionary_definition = definition_response.body
     definition = dictionary_definition[0]["text"]
 
     p "The definition of the word you requested is: " + definition
 
     examples_response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-    dictionary_examples = response.body
+    dictionary_examples = examples_response.body
     top_example = dictionary_examples["examples"][0]["text"]
 
     p "Here is an example of how you can utilize the word you requested: " + top_example
 
     pronunciation_response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-    dictionary_pronunciation = response.body
+    dictionary_pronunciation = pronunciation_response.body
     pronunciation = dictionary_pronunciation[0]["raw"]
 
     p "Here is how you pronounce the word you requested: " + pronunciation
